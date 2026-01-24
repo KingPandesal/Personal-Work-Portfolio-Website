04d63f6916c2f5ca05728b69a3e5936f45140246
SixxCodes <z.madayag.141776.tc@umindanao.edu.ph>
2026-01-24 23:49:17 +0800

    Fixed: home file. Add: Bubble animations.

--- js/main.js
+++ js/main.js
@@ -5,6 +5,6 @@ const openOffScreenMenu = document.querySelector('.ham-menu');
 
 openOffScreenMenu.addEventListener('click', () => {
     openOffScreenMenu.classList.toggle('active');
-    offScreenMenu.classList.toggle('active');
+    offScreenMenu.classList.toggle('active'); 
 });
 // </Sidebar Menu (Mobile)>
