d641c3179876b2f15f1641849ccfaa55a8e11eec
SixxCodes <z.madayag.141776.tc@umindanao.edu.ph>
2026-04-04 11:59:08 +0800

    Finish: Setup Tailwind css from just cdn.

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+export default {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          light: '#fce7f3',
+          DEFAULT: '#db2777',
+          dark: '#9d174d',
+        }
+      }
+    },
+  },
+  plugins: [],
+}
